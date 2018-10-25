4b43a78af629c60bfcc10b0822c98a1693337bb3
Steve Meads <smeads@sofi.org>
2018-10-24 21:44:02 -0600

    Add src folder

---
+++ src/index.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+  const element = document.createElement("h1");
+  element.innerHTML = "JavaScript 30 Challenge";
+  document.body.appendChild(element);
+});
