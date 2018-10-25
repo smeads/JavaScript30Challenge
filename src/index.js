ddd3f08419e9136ef214b2d78fe7576bdefb63f4
Steve Meads <smeads@sofi.org>
2018-10-25 17:16:59 -0600

    Update to be very basic

--- src/index.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-  const element = document.createElement("h1");
-  element.innerHTML = "JavaScript 30 Challenge";
-  document.body.appendChild(element);
-});
